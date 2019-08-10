55506ee10864e3db54517231e82476b4b26cc542
doever <1262014090@qq.com>
2019-08-10 22:32:51 +0800

    test schedure middle

--- rmms/test.py
+++ rmms/test.py
@@ -6,7 +6,34 @@ __date__ = '2019/8/10 19:41'
 import os
 import sys
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(os.path.abspath(__file__))
-print(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# print(os.path.abspath(__file__))
+# print(os.path.dirname(os.path.abspath(__file__)))
+# print(BASE_DIR)
+
+# print(locale.getpreferredencoding(False))
+
+import sched, time
+
+s = sched.scheduler(time.time, time.sleep)
+
+
+def print_time(a='default'):
+    print("From print_time", time.time(), a)
+
+
+def print_some_times():
+    print(time.time())
+    s.enter(10, 1, print_time)
+    s.enter(5, 2, print_time, argument=('positional',))
+    s.enter(5, 1, print_time, kwargs={'a': 'keyword'})
+    s.run()
+    print(time.time())
+
+def handle_schedule():
+    s.enter(3, 1, print_time)
+    print(time.time())
+    s.run()
+
+while True:
+    handle_schedule()
