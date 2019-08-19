b7afc6a035cb176a6ee493b66b897cb96f376366
doever <1262014090@qq.com>
2019-08-19 22:17:52 +0800

    完成交互式逻辑，简单完成新增文章前后台处理代码

--- rmms/essay/view.py
+++ rmms/essay/view.py
@@ -3,6 +3,9 @@
 __author__ = 'doever'
 __date__ = '2019/8/9 22:46'
 
+import time
+from datetime import datetime
+
 from rmms import restful
 from rmms.shortcuts import renter, json_response
 from rmms.essay.model import db
@@ -41,7 +44,18 @@ def pub_essay(request):
     if request.method == "GET":
         return renter("essay:pub_essay.html")
     else:
-        return renter("essay:essay_detail.html")
+        title = request.POST.get('title')
+        category = request.POST.get('category')
+        thumbnail = request.POST.get('thumbnail')
+        content = request.POST.get('content')
+        way = request.POST.get('way')
+        author = request.POST.get('author')
+        # 后期增加form模块过滤
+        id = int(time.time())
+        userid = request.user.get('userid')
+        date = str(datetime.now())[0:19]
+        db.insert(f"insert into essay values({id},{userid},'{thumbnail}','{title}','{content}','{category}',100,0,100,'1','{date}',0,0,'','','')")
+        return restful.ok(message=id)
 
 
 if __name__ == '__main__':
