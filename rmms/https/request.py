ccb36b2533a128853b180dfd62d25473cf54b162
doever <1262014090@qq.com>
2019-08-22 20:19:30 +0800

    分离请求与响应，重写response模块、

--- rmms/https/request.py
+++ rmms/https/request.py
@@ -3,53 +3,48 @@ from datetime import datetime
 from urllib import parse
 
 
-
 class Request():
-	def __init__(self,environ):
+	def __init__(self, environ):
 		self._clean_request(environ)
-
 
 	def _clean_request(self, environ):
 		'''
 		构造请求参数，绑定实例对象上
 		'''
-	    self.method = environ.get('REQUEST_METHOD')
-	    self.path = environ.get('PATH_INFO')
-	    self.content_type = environ.get('CONTENT_TYPE')
-	    self.length = environ.get('CONTENT_LENGTH') or 0
-	    self.GET = Request.parse_di(environ.get('QUERY_STRING'))
-	    self.POST = Request.parse_di(environ.get('wsgi.input').read(int(self.length)).decode())
-	    self.POST = {k: Request.url_decode(v) for k, v in self.POST.items()}
-	    self.cookie = environ.get('HTTP_COOKIE') or ''
-	    self.user = parse_di(self.cookie)
-
+		self.method = environ.get('REQUEST_METHOD')
+		self.path = environ.get('PATH_INFO')
+		self.content_type = environ.get('CONTENT_TYPE')
+		self.length = environ.get('CONTENT_LENGTH') or 0
+		self.GET = Request.parse_di(environ.get('QUERY_STRING'))
+		self.POST = Request.parse_di(environ.get('wsgi.input').read(int(self.length)).decode())
+		self.POST = {k: Request.url_decode(v) for k, v in self.POST.items()}
+		self.cookie = environ.get('HTTP_COOKIE') or ''
+		self.user = Request.parse_di(self.cookie)
 
 	@staticmethod
 	def parse_di(url):
 		'''
 		对类似get参数的字符串解析成字典
 		'''
-	    if url:
-	        li = url.split('&')
-	        new_li = [[i.split("=")[0], i.split("=")[1]] for i in li]
-	        return dict(new_li)
-	    return {}
+		if url:
+			li = url.split('&')
+			new_li = [[i.split("=")[0], i.split("=")[1]] for i in li]
+			return dict(new_li)
+		return {}
 
+	@staticmethod
+	def url_decode(_s):
+		'''urlencode解码'''
+		if _s:
+			d = "keys=" + _s
+			return parse.parse_qs(d)['keys'][0]
+		return _s
 
-    @staticmethod
-    def url_decode(_s):
-        '''urlencode解码'''
-        if _s:
-            d = "keys=" + _s
-            return parse.parse_qs(d)['keys'][0]
-        return _s
-
-
-	def print_environ(self, environ):
+	def print_environ(self):
 		'''
 		打印请求的环境变量
 		'''
 		print("*"*50)
-		for k, v in environ.items():
-	        print(k+":"+str(v))
-	    print("*"*50)
+		for k, v in self.environ.items():
+			print(k+":"+str(v))
+		print("*"*50)
---
+++ rmms/https/response.py
@@ -0,0 +1,38 @@
+import re
+import json
+
+
+class ResponseBase(dict):
+    def __init__(self, code=None, content_type=None, content=None, kwargs=None):
+        self.code = code
+        self.content_type = content_type
+        self.content = content
+        if kwargs and isinstance(kwargs, dict):
+            for k, v in kwargs.items():
+                self.k = v
+
+    def error_404(self):
+        self.code = '404 not found'
+        self.content = "<h1>the page you request was stolen by martians...</h1>"
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(f"没有属性{key}")
+
+    def __setattr__(self, key, value):
+        # 不可写成self.key=value,会造成无穷递归
+        self[key] = value
+
+
+class HttpResponse(ResponseBase):
+    def __init__(self, content=None, code='200 ok', content_type='text/plain', kwargs=None):
+        super().__init__(code, content_type, content, kwargs)
+
+
+class JsonResponse(ResponseBase):
+    def __init__(self, content=None, code='200 ok', content_type='json', kwargs=None):
+        assert isinstance(content, dict), "json序列化需要使用dict"
+        super().__init__(code, content_type, json.dumps(content), kwargs)
+
