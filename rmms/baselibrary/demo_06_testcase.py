4e1f363eeeb66d8a7735d22c1ded9bff173339e2
doever <1262014090@qq.com>
2019-08-26 22:16:07 +0800

    新增后台form验证表单模块，功能尚未实现

---
+++ rmms/baselibrary/demo_06_testcase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+__author__ = 'doever'
+__date__ = '2019/8/26 22:11'
+'''单元测试库'''
+
+
+if __name__ == '__main__':
+    pass
---
+++ rmms/essay/forms.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+__author__ = 'doever'
+__date__ = '2019/8/26 22:12'
+
+
+# data = EssayForm(request.Post)
+# if data.is_valid():
+#     data.save()
+# else:
+#     error_message = {"essayid": ['请输入id号', '字段长度不能少于..']}
+
+class EssayForm():
+    def __init__(self):
+        pass
+
+    # essayid = forms.ChardFiled()
+
+
+if __name__ == '__main__':
+    pass
