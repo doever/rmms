d8abd4f4823e0b05b2cce0cacc973922604f6623
doever <1262014090@qq.com>
2019-09-26 21:28:43 +0800

    pillow验证码示例代码

---
+++ rmms/baselibrary/demo_11_pillow.py
@@ -0,0 +1,39 @@
+import random
+import time
+
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+
+
+# 随机字母:
+def rndChar():
+    return chr(random.randint(65, 90))
+
+
+# 随机颜色1:
+def rndColor():
+    return random.randint(64, 255), random.randint(64, 255), random.randint(64, 255)
+
+
+# 随机颜色2:
+def rndColor2():
+    return random.randint(32, 127), random.randint(32, 127), random.randint(32, 127)
+
+width = 60 * 4
+height = 60
+image = Image.new('RGB', (width, height), (255, 255, 255))
+# 创建Font对象:
+font = ImageFont.truetype('C:/Windows/Fonts/Arial.ttf', 36)
+# 创建Draw对象:
+draw = ImageDraw.Draw(image)
+# 填充每个像素:
+for x in range(width):
+    for y in range(height):
+        draw.point((x, y), fill=rndColor())
+# 输出文字:
+for t in range(4):
+    draw.text((60 * t + 10, 10), rndChar(), font=font, fill=rndColor2())
+# 模糊:
+image = image.filter(ImageFilter.BLUR)
+image_name = str(int(time.time()))
+image.save(f'./basefile/code{image_name}.jpg', 'jpeg')
+
